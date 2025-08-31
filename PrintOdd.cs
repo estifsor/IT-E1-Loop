156ef64cf6681641260fe64a4d8636e59952bd01
ESTIFANE M. SORIO <202080119@psu.palawan.edu.ph>
2025-08-31 21:35:45 +0800

    Add files via upload

---
+++ PrintOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print Odd Numbers: Write a program to print all odd numbers between 1 and 50. 
+            int i = 1;
+            while (i <= 50)
+            {
+                Console.WriteLine(i);
+                i = i + 2;
+            }
+            
+
+        }
+    }
+}
