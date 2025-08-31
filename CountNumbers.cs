a2159ef70e4458342a77bb918de7e00d7638cdc4
ESTIFANE M. SORIO <202080119@psu.palawan.edu.ph>
2025-08-31 21:35:14 +0800

    Add files via upload

---
+++ CountNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Count Down from 10 to 1: Write a program that counts down from 10 to 1 using a loop.
+
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
