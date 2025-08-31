630aa68699c2d23c87e16c35997326773817f576
ESTIFANE M. SORIO <202080119@psu.palawan.edu.ph>
2025-08-31 21:54:28 +0800

    Add files via upload

---
+++ SumOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sum of Odd Digits: Write a program to calculate the sum of odd digits in a given number.
+
+            int i = 1;
+            int sum = 0;
+            while (i <= 50)
+            {
+                sum = sum + i;
+                Console.WriteLine(sum);
+                i = i + 2;
+            }
+        }
+    }
+}
