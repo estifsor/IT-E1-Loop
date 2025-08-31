d1e7f638c7c529a8ddde0bb970d2146a4c4f34aa
ESTIFANE M. SORIO <202080119@psu.palawan.edu.ph>
2025-08-31 21:53:50 +0800

    Add files via upload

---
+++ SquareNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Sum of Squares: Write a program to calculate the sum of the squares of the first 10 natural numbers.
+            for (int i = 1; i <= 10; i++)
+            {
+                int sum = i*i;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
